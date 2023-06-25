e0dbae189b4c47fc87a60e5ce4ebc026f8aa14b6
Gonza López <gonzagonzalopez20@gmail.com>
2023-06-24 22:58:07 -0300

    Change colores de botones especiales

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -2,18 +2,31 @@ import React from "react";
 import "../style-sheets/Boton.css";
 
 function Boton(props) {
+  
+	let claseAdicional;
+  if (esOperador(props.children)) {
+    claseAdicional = "operador";
+  } else if (esBotonEspecial(props.children)) {
+		claseAdicional = "boton-especial";
+  } else {
+		claseAdicional = "";
+	}
 
-	const esOperador = valor => {
-		return valor === '+' || valor === '-' || valor === '*' || valor === '/';
-	};
+  return (
+    <div
+      className={`boton-contenedor ${claseAdicional}`.trimEnd()}
+      onClick={() => props.manejarClick(props.children)}>
+      {props.children}
+    </div>
+  );
+}
 
-	return (
-		<div
-			className={`boton-contenedor ${esOperador(props.children) ? "operador": ""}`.trimEnd()}
-			onClick={() => props.manejarClick(props.children)}>
-			{props.children}
-		</div>
-	);
+function esOperador(valor) {
+  return valor === "+" || valor === "-" || valor === "*" || valor === "/";
+}
+
+function esBotonEspecial(valor) {
+	return valor === "AC" || valor === '%' || valor === '=';
 }
 
 export default Boton;
